2bd45c795586a9ac690846f2905e847eab912229
miguel jimenez <denkatalabeno@gmail.com>
2018-11-20 13:45:18 +0100

    sessions are now authd, users also generate simple tokens

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+class V1::SessionsController < Devise::SessionsController
+    def create
+        @user = User.find_by_email(user_params[:email])
+
+        if @user && @user.valid_password?(user_params[:password])
+          @current_user = @user
+          render :user 
+        else
+          render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy #need to check what is this respond_to shit
+        if @current_user
+            message = "session for user has been destroyed"
+            @current_user = nil
+            render json: { message: { message => ['please proceed'] } }, status: :ok
+        end
+        render json: { message: { message => ['please proceed'] } }, status: :ok
+    end
+
+    def user_params
+        params.permit!
+    end
+end
---
+++ app/views/v1/sessions/user.json.jbuilder
@@ -0,0 +1,4 @@
+json.data do
+    json.name @user.name
+    json.surname @user.surname
+end
