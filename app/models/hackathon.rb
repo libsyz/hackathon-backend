072cdc638dc29423ad30d79b47d8166ca996036b
miguel jimenez <denkatalabeno@gmail.com>
2018-11-25 23:08:29 +0100

    database is in a better place

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -12,11 +12,11 @@ class Hackathon < ApplicationRecord
     end
 
     def generate_phases
-        problem_statement = HackathonPhase.create(type:"DefinePhase") 
-        empathise = HackathonPhase.create(type:"EmpathisePhase")
-        ideate = HackathonPhase.create(type:"IdeatePhase")
-        prototype = HackathonPhase.create(type:"PrototypePhase")
-        test_protocol = HackathonPhase.create(type:"TestPhase")
+        problem_statement = HackathonPhase.create(type:"DefinePhase", phase_number: 1) 
+        empathise = HackathonPhase.create(type:"EmpathisePhase", phase_number: 2)
+        ideate = HackathonPhase.create(type:"IdeatePhase", phase_number: 3)
+        prototype = HackathonPhase.create(type:"PrototypePhase", phase_number: 4)
+        test_protocol = HackathonPhase.create(type:"TestPhase", phase_number: 5)
         hackathon_phases << problem_statement << empathise << ideate << prototype << test_protocol
         save
     end
